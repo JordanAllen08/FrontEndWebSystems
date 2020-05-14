8eee98f3a792eb8f7bc32138fa3fd46ca613ab30
Jordan <ALLENJT6055@UWEC.EDU>
2020-05-14 08:49:06 -0500

    Added basic compnents
    *added components todo store work and reducer work

--- to-dolist/src/index.js
+++ to-dolist/src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './component/App';
+//enables connected states
+import {Provider} from 'react-redux';
+//combines states into one
+import {createStore} from 'redux';
+import rootReducer from './reducers'
 import * as serviceWorker from './serviceWorker';
+
+const store = createStore(rootReducer);
+
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={ store }>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
